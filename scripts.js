67c4fb7d8fef71151254993de7c2298cb6ef557c
ray4students <142702156+ray4students@users.noreply.github.com>
2023-08-19 18:46:31 -0700

    Create scripts.js

---
+++ scripts.js
@@ -0,0 +1,32 @@
+// scripts.js
+// scripts.js
+
+// Show or hide the "Scroll to Top" button based on scroll position
+window.onscroll = function () {
+    scrollFunction();
+};
+
+function scrollFunction() {
+    const scrollToTopBtn = document.getElementById('scrollToTopBtn');
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        scrollToTopBtn.style.display = 'block';
+    } else {
+        scrollToTopBtn.style.display = 'none';
+    }
+}
+
+// Scroll to top when the button is clicked
+function scrollToTop() {
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE, and Opera
+}
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
