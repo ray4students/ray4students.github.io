5bde82215b0e6b9c6a63db80776cdc64faf7876c
ray4students <142702156+ray4students@users.noreply.github.com>
2023-08-20 15:14:22 -0700

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,1 +1,13 @@
+// Add smooth scrolling to navigation links
+document.querySelectorAll('.nav-links a').forEach(link => {
+    link.addEventListener('click', function (event) {
+        event.preventDefault();
+        const targetId = this.getAttribute('href');
+        const targetSection = document.querySelector(targetId);
+        window.scrollTo({
+            top: targetSection.offsetTop,
+            behavior: 'smooth'
+        });
+    });
+});
 
