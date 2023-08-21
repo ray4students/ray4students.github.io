5e6d46b6323f6fae2ae91249e4bf6bf2695a242d
ray4students <142702156+ray4students@users.noreply.github.com>
2023-08-20 18:01:25 -0700

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,22 +1,11 @@
-const hamburger = document.getElementById('hamburger');
-const navLinks = document.getElementById('nav-links');
-const content = document.getElementById('content');
-
-hamburger.addEventListener('click', () => {
-    navLinks.classList.toggle('show');
-    hamburger.classList.toggle('open');
-});
-
-navLinks.addEventListener('click', () => {
-    navLinks.classList.remove('show');
-    hamburger.classList.remove('open');
-});
-
-document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-    anchor.addEventListener('click', function (e) {
-        e.preventDefault();
-
-        document.querySelector(this.getAttribute('href')).scrollIntoView({
+// Add smooth scrolling to navigation links
+document.querySelectorAll('.nav-links a').forEach(link => {
+    link.addEventListener('click', function (event) {
+        event.preventDefault();
+        const targetId = this.getAttribute('href');
+        const targetSection = document.querySelector(targetId);
+        window.scrollTo({
+            top: targetSection.offsetTop,
             behavior: 'smooth'
         });
     });
