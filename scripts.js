bb76c0ab69aecbaeb12d42de29ff3f26c3229120
ray4students <142702156+ray4students@users.noreply.github.com>
2023-08-20 17:03:22 -0700

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -10,33 +10,3 @@ document.querySelectorAll('.nav-links a').forEach(link => {
         });
     });
 });
-
-// Get all the navigation link elements
-const navLinks = document.querySelectorAll('.nav-link');
-
-// Define the content for each page
-const pageContents = {
-    index: `<div class="main-content">
-                <h1>Welcome to Your Multi-Page Website</h1>
-                <p>Explore the various sections using the navigation above.</p>
-            </div>`,
-    about: `<div class="section-content">
-                <h1>About Us</h1>
-                <p>This is where you can introduce yourself and your team.</p>
-            </div>`,
-    // ... Repeat for other pages
-};
-
-// Function to update the content based on the selected page
-function updateContent(page) {
-    const mainContent = document.querySelector('.main-content');
-    mainContent.innerHTML = pageContents[page];
-}
-
-// Add click event listeners to the navigation links
-navLinks.forEach(link => {
-    link.addEventListener('click', function() {
-        const page = this.dataset.page;
-        updateContent(page);
-    });
-});
