e5fed08b1b57652965b4d014fdb66e4de8ba9a31
ray4students <142702156+ray4students@users.noreply.github.com>
2023-09-10 13:53:15 -0700

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -2,31 +2,18 @@ document.addEventListener('DOMContentLoaded', function() {
     // Get references to the navigation links and page sections
     const navLinks = document.querySelectorAll('nav ul li a');
     const pages = document.querySelectorAll('.page');
-    
-    // Get a reference to the "Welcome to Educational Resources" title
-    const welcomeTitle = document.getElementById('welcome-title');
-    
-    // Function to hide all subpages
-    function hideSubpages() {
-        pages.forEach(page => {
-            page.style.display = 'none';
-        });
-    }
-    
-    // Function to handle title click (hide subpages)
-    welcomeTitle.addEventListener('click', function() {
-        hideSubpages();
-    });
-    
-    // Function to handle navigation link clicks (show corresponding subpage)
+
+    // Function to handle navigation link clicks
     function handleNavLinkClick(event) {
         // Prevent the default link behavior (navigation)
         event.preventDefault();
 
-        // Hide all subpages
-        hideSubpages();
+        // Hide all pages
+        pages.forEach(page => {
+            page.style.display = 'none';
+        });
 
-        // Show the selected subpage
+        // Show the selected page
         const targetId = event.target.getAttribute('href').substring(1); // Remove the '#' character
         const targetPage = document.getElementById(targetId);
         targetPage.style.display = 'block';
