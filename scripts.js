c9e661573c33b5191b8be8a439d0faca13e6d550
ray4students <142702156+ray4students@users.noreply.github.com>
2023-12-26 15:06:27 -0800

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -26,3 +26,27 @@ document.addEventListener('DOMContentLoaded', function() {
 });
 
 
+
+// Get the scroll-up button element
+var scrollUpButton = document.getElementById("scrollUpButton");
+
+// Show the button when the user scrolls down
+window.onscroll = function () {
+    scrollFunction();
+};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        scrollUpButton.style.display = "block";
+    } else {
+        scrollUpButton.style.display = "none";
+    }
+}
+
+// Scroll to the top when the button is clicked
+function scrollToTop() {
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE, and Opera
+}
+
+
